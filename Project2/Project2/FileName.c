a47caa560b50ab9591a1a9bedc463d05e6440f26
poluroslikm <133805160+poluroslikm@users.noreply.github.com>
2024-09-11 19:54:49 +0300

    Add files via upload

---
+++ Project2/Project2/FileName.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main() {
+    puts(" __  __     __  __     __  __  __  __ ");
+    puts(" __| __|   |  ||__     __||  ||  ||__ ");
+    puts("|__  __| o |__||__| o |__ |__||__||__|");
+}
